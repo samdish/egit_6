b2c5f772f87e13172237d2a1c876bb43ba9675f2
Samdish Arora <arora.samdish@gmail.com>
2016-06-08 15:49:13 +0530

    Initial commit for without autotools

---
+++ egit_6auto/src/functions.h
@@ -0,0 +1,26 @@
+/**
+ * @file functions.h
+ *
+ * @brief This is the header file for the project
+ *
+ *
+ */
+
+#define R 5
+#define C 5
+/**
+ * This function in used in reverse4.c for printing array elements in reverse spiral.
+ */
+void spiralPrint(int m, int n, int a[R][C]);
+
+/**
+ * This function is used in wildcard1.c to compare two strings.
+ */
+bool match(char *first, char * second);
+
+/*
+ * This function is used in wildcard.c to select strings and send them to match() method.
+ */
+void test(char *first, char *second);
+
+
